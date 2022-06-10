2825dba72139f342c32346e59f759a4065409c21
juaninicolai <juan0719@stud.kea.dk>
2022-06-10 16:21:17 +0200

    fix .env

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.congif();
+dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = process.env.MONGODB_URL;
-mongoose.connect(`${dbURI}, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true }`)
+mongoose.connect(`${dbURI}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
